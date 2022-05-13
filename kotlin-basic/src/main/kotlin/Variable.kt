1df93026e36a1ae576914b503be411255adf1ca4
paigeman <53284808+paigeman@users.noreply.github.com>
2022-05-13 09:18:58 +0800

    kotlin-basic: 1.variable

---
+++ kotlin-basic/src/main/kotlin/Variable.kt
@@ -0,0 +1,15 @@
+package org.fade.demo.kotlindemo.kotlinbasic
+
+val a: Int = 1
+// type can be inferred
+val b = 1
+// variables in top level must be initialized, and can not be
+// resigned in top level
+var e = 3
+
+fun main() {
+    val c: Int
+    c = 3
+    var d = 2
+    d = 3
+}
