9f42efb7bec3c22f7ed161460ed78910e50a1b2e
paigeman <53284808+paigeman@users.noreply.github.com>
2022-05-26 18:01:44 +0800

    kotlin-concept: 1.override method

--- kotlin-concept/src/main/kotlin/org/fade/demo/kotlindemo/kotlinconcept/clz/Inheritance.kt
+++ kotlin-concept/src/main/kotlin/org/fade/demo/kotlindemo/kotlinconcept/clz/Inheritance.kt
@@ -1,2 +1,37 @@
 package org.fade.demo.kotlindemo.kotlinconcept.clz
 
+open class Base(p: Int)
+
+class Derived(p: Int) : Base(p)
+
+class AnotherDerived : Base {
+
+    constructor(p: Int) : super(p) {
+
+    }
+
+}
+
+abstract class AbstractParent {
+
+    abstract fun doSomething()
+
+}
+
+class Child : AbstractParent() {
+    override fun doSomething() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+open class Shape {
+    open fun draw() { /*...*/ }
+    fun fill() { /*...*/ }
+
+}
+
+class Circle() : Shape() {
+    override fun draw() { /*...*/ }
+
+}
